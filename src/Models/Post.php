f801f7b15c60d0b28a4da2c88a667d9ff632fb63
ashrey <alberroteran@gmail.com>
2019-02-19 15:53:58 -0300

    Fixed delay

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -25,21 +25,27 @@ class Post extends Model
         return $this->belongsTo(config('chatter.user.namespace'));
     }
 
+    protected function defer(DOMDocument $doc, $tag, $src_blank)
+    {
+        $list = $doc->getElementsByTagName($tag);
+        foreach ($list as $i) {
+            if ($src = $i->getAttribute('src')) {
+                $i->setAttribute('src', $src_blank);
+                $i->setAttribute('data-src', $src);
+                $class =  $i->getAttribute('class');
+                $class = $class ? $class : '';
+                $i->setAttribute('class', trim("$class lazy"));
+                $i->setAttribute('data-src', $src);
+            }
+        }
+    }
+
     public function getBodyAttribute($value)
     {
         $doc = new DOMDocument();
         $doc->loadHTML("<?xml encoding=\"utf-8\" ?><div id='_content'>$value</div>");
-        $list = $doc->getElementsByTagName('img');
-        foreach ($list as $i) {
-            if ($src = $i->getAttribute('src')) {
-                $i->setAttribute('src', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
-                $i->setAttribute('data-src', $src);
-                $class =  $i->getAttribute('class');
-                $class = $class ? $class : '';
-                $i->setAttribute('class', "$class lazy");
-                $i->setAttribute('data-src', $src);
-            }
-        }
+        $this->defer($doc, 'img', 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');
+        $this->defer($doc, 'iframe', 'about:blank');
         return $doc->saveHTML($doc->getElementById('_content'));
     }
 }
